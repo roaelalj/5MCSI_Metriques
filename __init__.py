c0a954b999f6577acbcc9be694bba1aa7316e099
roaelalj <relalj@myges.fr>
2024-09-12 15:44:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,10 +9,7 @@ app = Flask(__name__)
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
-@app.route("/rapport/")
 
-def mongraphique():
-    return render_template("histogramme.html")
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2

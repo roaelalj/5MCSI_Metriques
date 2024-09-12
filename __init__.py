ae33688feaddcfd0216203f290f1837e75f2501e
roaelalj <relalj@myges.fr>
2024-09-12 14:30:33 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
+from datetime import datetime
+from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
-
-@app.route('/exercices/')
-def exercices():
-    return render_template('exercices.html')
-                                                                                                               
+    return render_template('hello.html')
+  @app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+  
 if __name__ == "__main__":
   app.run(debug=True)

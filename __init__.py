89634af600e708475a45d9fac25a16336fd6bd69
roaelalj <relalj@myges.fr>
2024-09-12 15:17:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,7 +11,10 @@ def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
 @app.route("/rapport/")
 def mongraphique():
-    return render_template("graphique.html")                                                                                                                                       
+    return render_template("graphique.html")   
+  @app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2

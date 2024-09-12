d68ed389406aa3002e9de1cb9280b0e85dcc88db
roaelalj <relalj@myges.fr>
2024-09-12 16:30:22 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,15 +4,17 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
+                                                                                                                                       
 app = Flask(__name__)                                                                                                                  
+                                                                                                                                       
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
- 
-@app.route('/contact/')
+
+@app.route("/contact/")
 def MaPremiereAPI():
     return render_template("contact.html")
- 
+
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -21,17 +23,33 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
- 
-@app.route('/rapport/')
+  
+@app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
- 
-@app.route('/histogramme/')
+
+@app.route("/histogramme/")
 def monhistogramme():
     return render_template("histogramme.html")
- 
+  
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+    try:
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.strftime('%Y-%m-%d %H:%M')  # Format minute
+        return jsonify({'minutes': minutes})
+    except ValueError:
+        return jsonify({'error': 'Invalid date format. Use the following format: YYYY-MM-DDTHH:MM:SSZ'}), 400
+  
+
+@app.route('/commits/')
+def commits_graph():
+    # Servir le fichier HTML pour le graphique
+    return render_template('commits.html')
+
+
 if __name__ == "__main__":
   app.run(debug=True)

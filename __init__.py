7a4facd6e4e1a25b7226ca1bfd2e64fd8655805c
roaelalj <relalj@myges.fr>
2024-09-12 16:08:26 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,15 +5,14 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
 app = Flask(__name__)                                                                                                                  
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"                                                                                                                                 
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
+ 
 @app.route('/contact/')
 def MaPremiereAPI():
     return render_template("contact.html")
+ 
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -25,9 +24,11 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+ 
 @app.route('/rapport/')
 def mongraphique():
     return render_template("graphique.html")
+ 
 @app.route('/histogramme/')
 def monhistogramme():
     return render_template("histogramme.html")

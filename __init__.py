d7fc69a4839e9e8c13a3f07f417ecfeca087514e
roaelalj <relalj@myges.fr>
2024-09-12 16:06:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,7 +11,9 @@ def MaPremiereAPI():
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
- 
+@app.route('/contact/')
+def MaPremiereAPI():
+    return render_template("contact.html")
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -20,12 +22,15 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-@app.route("/rapport/")
+@app.route('/rapport/')
 def mongraphique():
     return render_template("graphique.html")
+@app.route('/histogramme/')
+def monhistogramme():
+    return render_template("histogramme.html")
  
 if __name__ == "__main__":
   app.run(debug=True)
